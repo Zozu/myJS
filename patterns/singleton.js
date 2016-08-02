9cf6af9d11165951b529fb2a47efba14892acc14
Max Selekh <max.selekh@gmail.com>
2016-08-03 01:22:12 +0300

    singleton

---
+++ patterns/singleton.js
@@ -0,0 +1,25 @@
+//Patterns
+
+//#Singleton - in ES5
+
+//var1
+var singleton = {
+	num: 1,
+	one: function(){return this.num;}
+};
+
+//var2
+var Singleton = (function(){
+	var staticVar;
+	
+	function privateFunc() {
+		staticVar = 1;
+	};
+	function one() {
+		return staticVar;
+	};
+
+	return {
+		one: one()
+	};
+})();
