e4204bae8bee0ab4725306f544d0d0f21a977b45
Max Selekh <max.selekh@gmail.com>
2016-11-03 23:57:08 +0200

    bubble-sort.v2

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ let describe = require('mocha').describe,
 	chai = require('chai'),
 	bucketSort = require('./sortings/bucket-sort').BucketSort.Sorting,
 	SmallDigitHandler = require('./sortings/bucket-sort').BucketSort.handlers.SmallDigit,
-	binarySearch = require('./searches/binary-search.js').search;
+	//binarySearch
+	binarySearch = require('./searches/binary-search.js').search,
+	//bubbleSort
+	bubbleSort = require('./sortings/bubble-sort.js').sort;
 
 /*bucketSort([10,11,20])
 	.then(res => {console.log(res)})
 	.catch(err => {console.log(err)});*/
 
 let arr = [2,4,6,8,9,10,11,12,13,15,17,19,20],
+	unsortedArr = [3,6,3,7,1,9,3,2,1,0,2,12,175,5,2],
 	value = 4;
 
+console.log('binarySearch');
 console.log(binarySearch(arr, value));
+console.log('bubbleSort');
+console.log(bubbleSort(unsortedArr));
+
--- sortings/bubble-sort.js
+++ sortings/bubble-sort.js
@@ -1,48 +1,25 @@
 'use strict';
-function sortMax(start, end, array){
-	if(start == end) return array;
-
-	let temp, i;
-
-	for(i = start; i < end; i++){
-		if(array[i] > array[i+1]) {
-			temp = array[i];
-			array[i] = array[i+1];
-			array[i+1] = temp;
-		}
-	}
-
-	return sortMin(start, end - 1, array);
-}
-
-function sortMin(start, end, array) {
-	if(start == end) return array;
-
-	let temp, i;
-
-	for (i = end; i > start; i--) {
-		if(array[i] < array[i-1]) {
-			temp = array[i];
-			array[i] = array[i-1];
-			array[i-1] = temp;
-		}
-	}
-
-	return sortMax(start + 1, end, array);
-}
 
 function sort(arr) {
-	return sortMax(0, arr.length - 1, arr);
+	let n = arr.length, 
+		i, temp,
+		newValue;
+
+	while(n != 0) {
+		newValue = 0;
+		for(i = 1; i <= n - 1; i++){
+			if(arr[i - 1] > arr[i]) {
+				temp = arr[i];
+				arr[i] = arr[i-1];
+				arr[i-1] = temp;
+				newValue = i;
+			}
+		}
+		n = newValue;
+	}
+	return arr;
 }
 
 module.exports = {
 	sort: sort
 }
-
-/**
- * Test
- */
-
-let myArr = [3,2,1,4,5,7,23,246,1,36,2,2,6,323];
-console.log(sort(myArr));
-
